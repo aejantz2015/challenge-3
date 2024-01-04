1ff1c7937770a84b55c64fd76f68a6da1ee3348c
Anna Jantzen <annajantzen@Annas-MacBook-Pro.local>
2024-01-04 17:59:11 -0600

    updated code

---
+++ script.js
@@ -0,0 +1,54 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "q", "x", "y", "z"];
+var char = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "=", ".", ",", "/"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+function generatePassword () {
+  var passwordOption = [];
+  var passwordLength = Number(prompt("How long would you like for your password to be? 8-128"))
+
+  if(isNaN(passwordLength) || (passwordLength < 8) || (passwordLength > 128)) {
+    alert("Password must be between 8-128 charaters")
+    return
+  }
+  else {
+    numbers = confirm("Would you like your password to have numbers?");
+
+    lowerCase = confirm("Would you like your password to have lowercase letters?");
+
+    upperCase = confirm("Would you like your password to have uppercase letters?");
+
+    char = confirm("Would you like your password to have special characters?");
+
+  }
+
+  if (!numbers && !lowerCase && !upperCase && !char) {
+    alert("Must have one selection made")
+  }
+  
+
+
+  }
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+
+
+  passwordText.value = password;
+
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
